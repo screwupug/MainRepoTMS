588c8e7895c1fe717cb093719c5f933e87f4c82b
Kirill Grigorev <grigoriev@arcsinus.ru>
2025-03-02 20:58:32 +0400

    delete unused code

--- src/Main.java
+++ src/Main.java
@@ -3,67 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("Hello", 12);
-        map.put("Hello1", 12);
-        map.put("Hello2", 12);
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        List<Integer> doubledList = list.stream()
-                .map(n -> n * 2)
-                .toList();
-
-        deleteEveryTwo();
-
     }
-
-    public static void deleteEveryTwo() {
-        Scanner scn = new Scanner(System.in);
-        LinkedList<Integer> peoples = new LinkedList<>();
-        try {
-            int numberOfPeople = scn.nextInt();
-
-            for (int i = 1; i <= numberOfPeople; i++) {
-                peoples.addLast(i);
-            }
-
-            int index = 1;
-            System.out.println("Начальный круг людей: " + peoples);
-
-            while (peoples.size() > 1) {
-                Iterator<Integer> iterator = peoples.iterator();
-                while (iterator.hasNext()) {
-                    iterator.next();
-                    if (index % 2 == 0) iterator.remove();
-                    index++;
-                }
-                System.out.println("Промежуточный результат: " + peoples);
-            }
-
-            System.out.println("Оставшийся человек: " + peoples);
-        } catch (Exception ex) {
-            System.out.println("Произошла ошибка!");
-        }
-    }
-
-
-
-
-    private static void iterateMap(Map<String, Integer> map) {
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-        }
-    }
-
-
-
 }
 
 
--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,3 @@
-import java.util.*
-
 fun main() {
-    // Неизменяемая коллекция
-    val immutableList = listOf(1, 2, 3)
-//    immutableList.add(4) // Нельзя добавить элемент
-    // Делаем коллекцию изменяемой (создается новая коллекция типа ArrayList)
-    val mutableImmutableList = immutableList.toMutableList()
-    mutableImmutableList.add(4)
-
-    // Изменяемая коллекция
-    val mutableList = mutableListOf(1, 2, 3)
-    mutableList.add(4)
-
-    // Пустая коллекция (неизменяемая)
-    val emptyList = emptyList<Int>()
-
-    // Классический ArrayList из Java
-    val arrayList = ArrayList<Int>(Arrays.asList(1, 2, 3))
-    arrayList.add(4)
-
-    mutableList.map {
-        it * 2
-    }
-
-    println(mutableList)
-
 }
 
